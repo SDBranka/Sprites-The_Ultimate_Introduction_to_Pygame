cd9084f46be550dca068c91afacb8a8958886fe1
Sam Branka <sdbranka@me.com>
2021-09-15 05:00:14 -0400

    obstacle as sprite drawn to screen

--- main.py
+++ main.py
@@ -50,6 +50,35 @@ class Player(pygame.sprite.Sprite):
         self.animation_state()
 
 
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+        if type == "fly":
+            fly_frame_1 = pygame.image.load("img/fly/fly1.png").convert_alpha()
+            fly_frame_2 = pygame.image.load("img/fly/fly2.png").convert_alpha()
+            self.frames = [fly_frame_1, fly_frame_2]
+            y_pos = 210
+        else:
+            snail_frame_1 = pygame.image.load("img/snail/snail1.png").convert_alpha()
+            snail_frame_2 = pygame.image.load("img/snail/snail2.png").convert_alpha()
+            self.frames = [snail_frame_1, snail_frame_2]
+            y_pos = 300
+        
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(100, 200), y_pos))
+
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def update(self):
+        self.animation_state()
+
+
+
 # function to consistantly update and redraw score_surface
 def display_score():
     current_time = (pygame.time.get_ticks() - start_time) / 1000
@@ -125,8 +154,18 @@ start_time = 0
 # store the score for global use
 score = 0
 
+# create a GroupSingle instance of Player 
 player = pygame.sprite.GroupSingle()
 player.add(Player())
+
+# create a sprite group for obstacles
+obstacle_group = pygame.sprite.Group()
+
+
+
+
+
+
 
 
 # create a surface with an image
@@ -221,11 +260,20 @@ while True:
             # timers
             # enemy/obstacle deployment timer 
             if event.type == obstacle_timer:
+                # add an instance of Obstacle to obstacle_group
+                obstacle_group.add(Obstacle("fly"))
+
+
+
                 # build random obstacle type generator; randint 0/1, if 1 snail, if 0 fly
                 if randint(0, 2):
                     obstacle_rect_list.append(snail_surface.get_rect(bottomright = (randint(900, 1100), 300)))
                 else:
                     obstacle_rect_list.append(fly_surface.get_rect(bottomright = (randint(900, 1100), 210)))
+            
+            
+            
+            
             # obstacle animaton timers
             if event.type == snail_animation_timer:
                 if snail_frame_index == 0:
@@ -276,11 +324,13 @@ while True:
         screen.blit(player_surface, player_rect)
         player.draw(screen)
         player.update()
+        obstacle_group.draw(screen)
 
 
 
         # collisions
         game_active = collisions(player_rect, obstacle_rect_list)
+    # if game_active is false(new game/player death) display intro/game over screen
     else:
         # clear all existing enemies
         obstacle_rect_list.clear()
