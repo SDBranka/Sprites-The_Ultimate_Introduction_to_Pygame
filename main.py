b08f0cc638d18caee23e39daf9ca88ff61af1d9b
Sam Branka <sdbranka@me.com>
2021-09-15 01:42:30 -0400

    converting to player sprite

--- main.py
+++ main.py
@@ -4,6 +4,52 @@ import math
 from random import randint
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        # animating the player
+        player_walk_1 = pygame.image.load("img/player/player_walk_1.png").convert_alpha()
+        player_walk_2 = pygame.image.load("img/player/player_walk_2.png").convert_alpha()
+        # list containing the two walk animation pics controlled by player_index
+        self.player_index = 0
+        self.player_jump = pygame.image.load("img/player/jump.png").convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom = (200, 300))
+        self.gravity = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        # if player is on the ground and space bar is pressed player jump
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+            self.gravity = -20
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        # creates appearance of solid floor
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+
+    def animation_state(self):
+        # display jump surface when player is in the air
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        # play walking animation if the player is on the floor    
+        else:
+            # increasing the index by small increments extends how many frames an index position is displayed for
+            self.player_index += 0.1
+            if self.player_index >= len(player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+
 # function to consistantly update and redraw score_surface
 def display_score():
     current_time = (pygame.time.get_ticks() - start_time) / 1000
@@ -78,6 +124,10 @@ start_time = 0
 
 # store the score for global use
 score = 0
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
 
 # create a surface with an image
 sky_surface = pygame.image.load('img/Sky.png').convert()
@@ -162,7 +212,7 @@ while True:
             # make player jump using space bar
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                    player_gravity = -20
+                    player_gravity = -27
             # make player jump if player is clicked (1:43:15)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
@@ -224,6 +274,10 @@ while True:
         player_animation()
         # display player
         screen.blit(player_surface, player_rect)
+        player.draw(screen)
+        player.update()
+
+
 
         # collisions
         game_active = collisions(player_rect, obstacle_rect_list)
